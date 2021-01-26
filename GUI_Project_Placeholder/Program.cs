94e9bd9e4490ebb6950849a565ed18794235912c
Kyle Rolland <kylwroll@ut.utm.edu>
2021-01-26 15:10:31 -0600

    Add project files.

---
+++ GUI_Project_Placeholder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Project_Placeholder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
